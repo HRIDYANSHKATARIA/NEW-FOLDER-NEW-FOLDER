3c4d1838522a45cd8d8dac5ba38727519f318646
HRIDYANSHKATARIA <85940668+HRIDYANSHKATARIA@users.noreply.github.com>
2022-03-14 19:02:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,115 @@
+song = "";
+  scorerightWrist = 0;
+  leftWristX = 0; 
+  leftWristY = 0; 
+  rightWristX = 0; 
+  rightWristY = 0; 
+  scoreleftWrist = 0 ;
+function setup()
+{
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+    
+    poseNet = ml5.poseNet(video,modelloaded);
+    poseNet.on('pose',gotposes);
+}
+
+function draw()
+{
+    image(video,0,0,600,500);
+
+    fill("red");
+    stroke("red");
+
+if(scorerightWrist > 0.2){
+    circle(rightWristX,rightWristY,20);
+  
+    if(rightWristY > 0 && rightWristY <= 100)
+    {
+     document.getElementById("speed").innerHTML = "Speed = 0.5X";
+     song.rate(0.5);
+    }
+    else if(rightWristY > 100 && rightWristY <= 200)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 1X"; 
+        song.rate(1);
+    }
+    else if(rightWristY > 200 && rightWristY <= 300)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 1.5X";
+        song.rate(1.5);
+    }
+    else if(rightWristY > 300 && rightWristY <= 400)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 2X";
+        song.rate(2);
+    }
+    else if(rightWristY > 400 && rightWristY <= 500)
+    {
+        document.getElementById("speed").innerHTML = "Speed = 2.5X";
+        song.rate(2.5);
+    }
+}
+
+    if(scoreleftWrist > 0.2)
+    {
+    circle(leftWristX,leftWristY,20);
+
+    InnumberleftwristY = Number(leftWristY);
+    removedecimals = floor(InnumberleftwristY);
+    volume = removedecimals/500;
+
+    document.getElementById("volume").innerHTML = "Volume = "+volume;
+    song.setVolume(volume);
+
+    
+    }
+}
+
+function preload()
+{
+    song = loadSound("music.mp3");
+
+}
+
+function play()
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function modelloaded()
+{
+    console.log('POSENET IS INITIALIZED');
+}
+
+function gotposes(results)
+{
+    if (results.length > 0)
+    {
+        console.log(results);
+
+        scoreleftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreleftwrist ="+scoreleftWrist);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        
+        console.log("leftwrist X  = "+leftWristX);
+        console.log("leftwrist Y  = "+leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        
+        console.log("rightwrist X = "+rightWristX);
+        console.log("rightwrist Y = "+rightWristY);
+
+        scorerightWrist = results[0].pose.keypoints[10].score; 
+        console.log("scorerightwrist ="+scorerightWrist);
+}
+
+}
